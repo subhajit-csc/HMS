47c34a617c7b0a603e745a8e33e44743a4acf7d6
Subhajit Manna <subhajit-csc@users.noreply.github.com>
2019-11-16 12:53:04 +0530

    initial set up commit
    initial set up commit

---
+++ src/main/java/org/hms/serviceapp/HmsApplication.java
@@ -0,0 +1,13 @@
+package org.hms.serviceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmsApplication.class, args);
+	}
+
+}
